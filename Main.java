94c7d364d3a083f9df03e5e1450baca7c3b109c2
maltebergmann <31304990+maltebergmann@users.noreply.github.com>
2017-08-24 11:23:22 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+   public static void main (String[] args) {
+   HelloWorld() ;
+   HW("Hello World!");
+}
+   
+   public static void HelloWorld() {
+      System.out.println ("Hello World!");
+      }
+   public static void HW(String text) {
+      System.out.print (text);
+   }      
+} 
